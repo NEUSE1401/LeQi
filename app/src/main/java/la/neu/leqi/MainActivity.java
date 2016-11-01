d1232755fb738499510393d3f6d512f6dc35b7d6
hu-yulin <709265369@qq.com>
2016-11-01 22:45:11 +0800

    侧滑栏图片已添加

--- app/src/main/java/la/neu/leqi/MainActivity.java
+++ app/src/main/java/la/neu/leqi/MainActivity.java
@@ -22,7 +22,7 @@ import la.neu.leqi.tools.image.ImageLoader;
  */
 public class MainActivity extends Activity {
 
-    private CarouselView CarouselView;
+    //private CarouselView CarouselView;
     private ImageView userFace;
     private DrawerLayout drawerLayout;
 
@@ -34,21 +34,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.first_page);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.main_title);
         //滚动广告栏
-        final CarouselView carouselView = (CarouselView) findViewById(R.id.vPager);
-        final ArrayList<String> viewList = new ArrayList<>();
-        viewList.add("http://neu.la/leqi/img/slider/Homeslider4.jpg");
-        viewList.add("http://neu.la/leqi/img/slider/Homeslider2.jpg");
-        viewList.add("http://neu.la/leqi/img/slider/Homeslider3.jpg");
-        viewList.add("http://neu.la/leqi/img/slider/Homeslider1.jpg");
-        viewList.add("http://neu.la/leqi/img/slider/Homeslider5.jpg");
-        final AdViewListener adViewListener = new AdViewListener(viewList,this,carouselView);
-        carouselView.setPageCount(5);
-        carouselView.setImageListener(adViewListener);
-        carouselView.addOnPageChangeListener(adViewListener);
-        //乐骑活动显示
-        final ImageLoader imageLoader = new ImageLoader(this);
-        final ImageView activity_pic = (ImageView) findViewById(R.id.activity_pic);
-        imageLoader.bindBitmap("http://neu.la/leqi/img/slider/Homeslider4.jpg",activity_pic);
+//        final CarouselView carouselView = (CarouselView) findViewById(R.id.vPager);
+//        final ArrayList<String> viewList = new ArrayList<>();
+//        viewList.add("http://neu.la/leqi/img/slider/Homeslider4.jpg");
+//        viewList.add("http://neu.la/leqi/img/slider/Homeslider2.jpg");
+//        viewList.add("http://neu.la/leqi/img/slider/Homeslider3.jpg");
+//        viewList.add("http://neu.la/leqi/img/slider/Homeslider1.jpg");
+//        viewList.add("http://neu.la/leqi/img/slider/Homeslider5.jpg");
+//        final AdViewListener adViewListener = new AdViewListener(viewList,this,carouselView);
+//        carouselView.setPageCount(5);
+//        carouselView.setImageListener(adViewListener);
+//        carouselView.addOnPageChangeListener(adViewListener);
+//        //乐骑活动显示
+//        final ImageLoader imageLoader = new ImageLoader(this);
+//        final ImageView activity_pic = (ImageView) findViewById(R.id.activity_pic);
+//        imageLoader.bindBitmap("http://neu.la/leqi/img/slider/Homeslider4.jpg",activity_pic);
 
         userFace= (ImageView) findViewById(R.id.user_face);
         drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
