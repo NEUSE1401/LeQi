afe645bf6f525ee7747d0212f5f6e7f8fe7b7f87
hu-yulin <709265369@qq.com>
2016-11-01 20:57:22 +0800

    首页，MainActivity

--- app/src/main/java/la/neu/leqi/MainActivity.java
+++ app/src/main/java/la/neu/leqi/MainActivity.java
@@ -2,6 +2,9 @@ package la.neu.leqi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
@@ -20,13 +23,15 @@ import la.neu.leqi.tools.image.ImageLoader;
 public class MainActivity extends Activity {
 
     private CarouselView CarouselView;
+    private ImageView userFace;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //自定义title
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first_page);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.main_title);
         //滚动广告栏
         final CarouselView carouselView = (CarouselView) findViewById(R.id.vPager);
@@ -44,5 +49,14 @@ public class MainActivity extends Activity {
         final ImageLoader imageLoader = new ImageLoader(this);
         final ImageView activity_pic = (ImageView) findViewById(R.id.activity_pic);
         imageLoader.bindBitmap("http://neu.la/leqi/img/slider/Homeslider4.jpg",activity_pic);
+
+        userFace= (ImageView) findViewById(R.id.user_face);
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        userFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
