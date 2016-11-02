51ae3185003c9efe86d0ec51c0bf129efa75d497
hu-yulin <709265369@qq.com>
2016-11-02 17:16:38 +0800

    个人信息界面完善

--- app/src/main/java/la/neu/leqi/PersonalInformationActivity.java
+++ app/src/main/java/la/neu/leqi/PersonalInformationActivity.java
@@ -1,13 +1,16 @@
 package la.neu.leqi;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by lenovo on 2016/11/2.
@@ -28,6 +31,9 @@ public class PersonalInformationActivity extends Activity{
     private TextView back_title;
     private ImageView imageView;
 
+    private int year=1995;
+    private int month=9;
+    private int day=21;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +66,15 @@ public class PersonalInformationActivity extends Activity{
         changeSex.setOnClickListener(new ChangePersonalInfoListener(sex.getText().toString()));
         changeContactWay.setOnClickListener(new ChangePersonalInfoListener(contactWay.getText().toString()));
 
+        changeAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(PersonalInformationActivity.this, Datelistener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+
     }
 
     class ChangePersonalInfoListener implements View.OnClickListener{
@@ -76,6 +91,30 @@ public class PersonalInformationActivity extends Activity{
         }
     }
 
+
+    private DatePickerDialog.OnDateSetListener Datelistener=new DatePickerDialog.OnDateSetListener()
+    {
+        /**params：view：该事件关联的组件
+         * params：myyear：当前选择的年
+         * params：monthOfYear：当前选择的月
+         * params：dayOfMonth：当前选择的日
+         */
+        @Override
+        public void onDateSet(DatePicker view, int myyear, int monthOfYear, int dayOfMonth) {
+            //修改year、month、day的变量值，以便以后单击按钮时，DatePickerDialog上显示上一次修改后的值
+            String remind;
+           if(myyear==year&&(monthOfYear+1)==month&&dayOfMonth==day){
+               remind="未改变";
+               Toast toast=Toast.makeText(PersonalInformationActivity.this,remind,Toast.LENGTH_LONG);
+               toast.show();
+               return;
+           }
+            year=myyear;
+            month=monthOfYear+1;
+            day=dayOfMonth;
+            age.setText(year+"-"+month+"-"+day);
+        }
+    };
 
 
 }
